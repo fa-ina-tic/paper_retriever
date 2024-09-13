57ebe355ae73fe380183895c6b3274332bf16888
soomin.lee <soomin.lee@maum.ai>
2024-09-13 15:52:09 +0900

    file_manager: implement directory watcher

---
+++ paper_retriever/src/file_manager/manager.py
@@ -0,0 +1,14 @@
+from handler import EventHandler
+
+class FileManager:
+  def __init__(self, handler:EventHandler):
+    self._list_dir = handler.file_list
+  
+  def upload_file():
+    return None
+  
+  def remove_file():
+    return None
+  
+  def change_file():
+    return None
