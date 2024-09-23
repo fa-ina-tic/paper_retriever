9d9b608963b3bbf8316c64bb2724b0a686dd0009
soomin.lee <soomin.lee@maum.ai>
2024-09-23 13:02:46 +0900

    paper_retriever/indexer: add ElasticsearchManager skeleton

---
+++ paper_retriever/src/indexer/Elasticsearch.py
@@ -0,0 +1,18 @@
+class ElasticIndexManager:
+  def _file_to_request(file_path):
+    pass
+
+  def index(index, file_path):
+    pass
+
+  def delete(index, body):
+    pass
+
+  def add(index, body):
+    pass
+
+  def reindex(index, target_index, body):
+    pass
+
+  def alias(index, alias):
+    pass
